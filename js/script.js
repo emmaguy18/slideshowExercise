30e8cb858168eb1ad8b045a0ff4253d7ab209ca4
Emma Guy <932400387@bun2-206-24.local>
2019-05-31 10:34:59 +1200

    fixed sliding

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-  //console.log("jquery has loaded");
-  $('.slideshowBttns').click(function(){
-    // console.log('button clicked')
-    setInterval(function(){
-      // $('#slideshowItem-1').first();
-      // console.log('this is the first slide');
-    }, 2000)
+  var slideshowContainter = $('.slideshowContainter');
+  setInterval(function(){
+
+    slideshowContainter.addClass('transition').css('transform', 'translateX(-1000px)');
+
+    setTimeout(function(){
+      var firstSlide = $('.slide').first();
+      slideshowContainter.append(firstSlide)
+      slideshowContainter.removeClass('transition').css('transform', 'translateX(0)');
+    }, 700)
+  }, 3000)
+
+
+  $('#nextBttn').click(function(){
+    var firstSlide = $('.slide').first();
+    //cloning method//
+    // var clonedSlide = firstSlide.clone();
+    // firstSlide.remove();
+    // slideshowContainter.append(clonedSlide);
+    slideshowContainter.append(firstSlide);
   });
 
 
-  $('#slideshowItem-1').first();
-  console.log('this is the first slide');
+
 
 
 });
